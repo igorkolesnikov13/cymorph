381fd1342efbe48d20a977be2687c34e7a7c7ee3
igorkolesnikov13 <igor.s.kolesnikov@gmail.com>
2022-05-06 07:32:24 -0300

    s review small fixes and comments

--- src/cymorph/cython_smoothness.pyx
+++ src/cymorph/cython_smoothness.pyx
@@ -7,6 +7,7 @@ from scipy.ndimage.filters import gaussian_filter
 cimport numpy as np
 cimport cython
 
+# butterworth smoothing functions
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
@@ -16,71 +17,74 @@ cpdef float butterworth(float d,float d0,float n):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-def filter_butterworth_2d(float[:,:] image, float smoothness_degradation, float butterworth_order, float max_d0):
-    # frac -> max smoothing at center
+def filter_butterworth_2d(float[:,:] image, float smoothness_degradation, float butterworth_order, float smoothing_area):
     cdef:
-        int heigth,width,cx,cy, i, j
-        float[:,:] smoothed
+        int heigth, width, i, j
+        float[:,:] smooothed_image
         np.complex64_t[:,:]  freq
 
     heigth, width = len(image),len(image[0])
-    cx, cy = width/2, heigth/2
 
-    smoothed = np.array([[0.0 for i in range(width)] for j in range(heigth)],dtype=np.float32)
+    smooothed_image = np.array([[0.0 for i in range(width)] for j in range(heigth)], dtype=np.float32)
     for i in range(heigth):
         for j in range(width):
-            smoothed[i][j] = image[i][j]
+            smooothed_image[i][j] = image[i][j]
             
 
     freq = fftpack.fft2(image)
     freq = fftpack.fftshift(freq)
+
     for i in range(heigth):
         for j in range(width):
-            freq[i][j] = freq[i][j] * butterworth(sqrt(((float(i) - float(heigth) / 2.0)*(float(i) - float(heigth) / 2.0)) + ((float(j) - float(width) / 2.0)*(float(j) - float(width) / 2.0))) ,smoothness_degradation * max_d0, butterworth_order)
-    smoothed = np.real(fftpack.ifft2(fftpack.ifftshift(freq))).astype(np.float32)
-    return smoothed
+            freq[i][j] = freq[i][j] * butterworth(sqrt(((float(i) - float(heigth) / 2.0)*(float(i) - float(heigth) / 2.0)) + ((float(j) - float(width) / 2.0)*(float(j) - float(width) / 2.0))) ,smoothness_degradation * smoothing_area, butterworth_order)
+    
+    smooothed_image = np.real(fftpack.ifft2(fftpack.ifftshift(freq))).astype(np.float32)
+    
+    return smooothed_image
 
-############################################ 
-#     Funcao Smoothness (Suavizacao):
-# entrada:
-#    mat - matriz do tipo numpy(list(list))
-#    kernel - matriz de convolucao
-#    corrFunction - funcao de correlacao (stats.pearsonr, stats.spearmanr, ....)
-# retorna:
-#    coeficiente encontrado (valor e prob. da hipotese nula),
-#    matriz,
-#    matrizRotacionada
+############################################
+# Smoothness (A) function:
+# input:
+#    image - 2d matrix of np.float32 type
+#    smoothed_function - 2d matrix containing smoothed image
+# return:
+#    original_image_pixels - pixels that was collected from original image and have pair with rotated immage
+#    rotated_image_pixels - pixels that was collected from rotated image and have pair with original immage
+#    final_image - image that contains only pixels that were collected (i.e pairs)
+#    collected_pixels_length - number of collected pixels in total
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-def get_smoothness(float[:,:] image, float[:,:] smoothed_image):
+def get_smoothness(float[:,:] image, float[:,:] smooothed_image):
     cdef:
-        int w, h, countPts, it, i, j
+        int w, h, valid_pixels, iteration, i, j, collected_pixels_length
         float[:,:] final_image
+        float[:] original_image_pixels, smoothed_image_pixels
     w, h = len(image[0]), len(image)
 
     final_image = np.zeros(shape=(w, h), dtype=np.float32)
-    countPts=0
+    valid_pixels = 0
     for i in range(w):
         for j in range(h):
             if(image[j, i] != 0.0):
-                countPts += 1
+                valid_pixels += 1
     
-    # É complicado analisar imagens muito pequenas, uma correlação com menos de 6 pontos não é estatisticamente relevante. 
-    # Não vejo este tipo de detalhe sendo discutido nos artigos clássicos de análise não paramétrica (como o do Conscelice), 
-    # por isso adotei um N de corte de acordo com o que achava melhor.
-    if(countPts<6):
-        return (305, 305, image) # flag value
-    it = 0 
+    # According to David(1938), a sample size equal or superior to 25 suffices.
+    if(valid_pixels < 25):
+        raise ValueError("Not enough valid pixels in image for correlation")
     
-    v1, v2 = [0.0 for i in range(countPts)],[0.0 for i in range(countPts)]
+    original_image_pixels = np.zeros(shape=valid_pixels, dtype=np.float32)
+    smoothed_image_pixels = np.zeros(shape=valid_pixels, dtype=np.float32)
 
+    iteration = 0 
     for i in range(w):
         for j in range(h):
-            if (image[j,i] != 0.0 and smoothed_image[j,i] != 0.0):
+            if (image[j,i] != 0.0) and (smooothed_image[j,i] != 0.0):
                 final_image[j,i] = image[j,i]
-                v1[it] = image[j,i]
-                v2[it] = smoothed_image[j,i]
-                it = it + 1
+                original_image_pixels[iteration] = image[j,i]
+                smoothed_image_pixels[iteration] = smooothed_image[j,i]
+                iteration = iteration + 1
 
-    return v1, v2, final_image
+    collected_pixels_length = iteration
+
+    return original_image_pixels, smoothed_image_pixels, final_image, collected_pixels_length
