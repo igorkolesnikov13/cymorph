45872e26b3524079811f8bbe867c7cdca909a76c
igorkolesnikov13 <igor.s.kolesnikov@gmail.com>
2022-05-06 07:32:12 -0300

    a review small fixes and comments

--- src/cymorph/asymmetry.py
+++ src/cymorph/asymmetry.py
@@ -2,7 +2,6 @@ from cymorph.cython_asymmetry import get_asymmetry
 from scipy.stats.stats import pearsonr, spearmanr
 import matplotlib.pyplot as plt
 
-
 class Asymmetry:
     """
     Asymmetry(image)
@@ -14,6 +13,7 @@ class Asymmetry:
     segmented_image : 2-d `~numpy.ndarray`
         Data array.
     """
+
     def __init__(self, segmented_image):
         if segmented_image.ndim != 2:
             raise ValueError("array must be 2-d")
@@ -24,13 +24,15 @@ class Asymmetry:
         if segmented_image.size == 0:
             raise ValueError("the size array can not be 0")
 
-
         self.segmented_image = segmented_image
         self._asymmetry()
-    
 
     def _asymmetry(self):
-        self.asymmetry_v1, self.asymmetry_v2, self.rotated_image, self.final_image, self.collected_points = get_asymmetry(self.segmented_image)
+        try:
+            self.asymmetry_v1, self.asymmetry_v2, self.rotated_image, self.final_image, self.collected_points = get_asymmetry(
+                self.segmented_image)
+        except ValueError:
+            raise ValueError("Not enough valid pixels in image for correlation")
 
     def get_collected_points_plot(self):
         """(Debugging routine) Correlation plot between original and rotated image"""
@@ -49,7 +51,7 @@ class Asymmetry:
     def get_pearsonr(self):
         """
         Pearson rank asymmetry coeficient
-         
+
         Returns:
             Pearson rank asymmetry coeficient : `float`
         """
@@ -59,7 +61,12 @@ class Asymmetry:
         return (1 - symmetry_pearsonr_correlation_coeficient)
 
     def get_spearmanr(self):
-        """Spearman rank asymmetry coeficient"""
+        """
+        Spearman rank asymmetry coeficient
+        
+        Returns:
+            Spearman rank asymmetry coeficient : `float`
+        """
 
         symmetry_spearmanr_correlation_coeficient = spearmanr(
             self.asymmetry_v1, self.asymmetry_v2)[0]
--- src/cymorph/cython_asymmetry.pyx
+++ src/cymorph/cython_asymmetry.pyx
@@ -4,36 +4,48 @@ cimport numpy as np
 cimport cython
 
 ############################################
-#     Funcoes de assimetria:
-# entrada:
-#    mat - matriz do tipo np(list(list))
-#    corrFunction - funcao de correlacao (stats.pearsonr, stats.spearmanr, ....)
-# retorna:
-#    coeficiente encontrado,
-#    matrizRotacionada
-#    pontos de correlacao (I,I_h)
-#    mascara de pontos considerados (rotacionado e nao rotacionado)
+# Asymmetry (A) function:
+# input:
+#    image - image - 2d matrix of np.float32 type
+# return:
+#    original_image_pixels - pixels that was collected from original image and have pair with rotated immage
+#    rotated_image_pixels - pixels that was collected from rotated image and have pair with original immage
+#    rotated_image - roteted version of original image
+#    final_image - image that contains only pixels that were collected (i.e pairs)
+#    collected_pixels_length - number of collected pixels in total
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 cpdef get_asymmetry(float[:,:] image):
     cdef:
          float[:,:]  rotated_image, final_image
-         int w, h, collected_points
+         int w, h, collected_pixels_length, valid_pixels
     w, h = len(image[0]), len(image)
 
     rotated_image = np.rot90(image,2)
     final_image = np.zeros(shape=(w, h), dtype=np.float32)
     
-    collected_points = 0
-    v1 = []
-    v2 = []
+    valid_pixels = 0
+    for i in range(w):
+        for j in range(h):
+            if(image[j, i] != 0.0):
+                valid_pixels += 1
+    
+    # According to David(1938), a sample size equal or superior to 25 suffices.
+    if(valid_pixels < 25):
+        raise ValueError("Not enough valid pixels in image for correlation")
+
+    collected_pixels_length = 0
+    original_image_pixels = []
+    rotated_image_pixels = []
+
     for i in range(w):
         for j in range(h):
             if (image[j,i] != 0) and (rotated_image[j,i] != 0):
                 final_image[j,i] = image[j,i]
-                v1.append(image[j,i])
-                v2.append(rotated_image[j,i])
-                collected_points = collected_points+1
+                original_image_pixels.append(image[j,i])
+                rotated_image_pixels.append(rotated_image[j,i])
     
-    return v1, v2, rotated_image, final_image, collected_points
+    collected_pixels_length = len(original_image_pixels)
+    
+    return original_image_pixels, rotated_image_pixels, rotated_image, final_image, collected_pixels_length
